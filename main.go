314bd708b8da08a16662a9db22f7b41f3f80b4fd
Christopher Kleine <chris@suletuxe.de>
2021-11-11 14:07:15 +0100

    Collition Detection

--- main.go
+++ main.go
@@ -15,11 +15,11 @@ var (
 		},
 		Direction: Point{X: 1, Y: 0},
 	}
-	frameCount = 0
-	prevState  uint8
-	fruit = Point{X: 10, Y: 10 }
-	rnd func(int) int
-   fruitSprite = [16]byte{0x00, 0xa0, 0x02, 0x00, 0x0e, 0xf0, 0x36, 0x5c, 0xd6, 0x57, 0xd5, 0x57, 0x35, 0x5c, 0x0f, 0xf0}
+	frameCount  = 0
+	prevState   uint8
+	fruit       = Point{X: 10, Y: 10}
+	rnd         func(int) int
+	fruitSprite = [16]byte{0x00, 0xa0, 0x02, 0x00, 0x0e, 0xf0, 0x36, 0x5c, 0xd6, 0x57, 0xd5, 0x57, 0x35, 0x5c, 0x0f, 0xf0}
 )
 
 func input() {
@@ -61,6 +61,21 @@ func update() {
 
 	if frameCount%15 == 0 {
 		snake.Update()
+
+		if snake.IsDead() {
+			snake.Body = []Point{
+				{X: 2, Y: 0},
+				{X: 1, Y: 0},
+				{X: 0, Y: 0},
+			}
+			snake.Direction = Point{X: 1, Y: 0}
+		}
+
+		if snake.Body[0].X == fruit.X && snake.Body[0].Y == fruit.Y {
+			snake.Body = append(snake.Body, snake.Body[len(snake.Body)-1])
+			fruit.X = rnd(20)
+			fruit.Y = rnd(20)
+		}
 	}
 	snake.Draw()
 
